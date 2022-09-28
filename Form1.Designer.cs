e6e3aafd03f168bb0fd8d9ee96cc667e77768f21
任延华 <renyh@dp2003.com>
2022-09-28 15:21:52 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace CountCodeLine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_dir = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox_ignore = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_only = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "目录";
+            // 
+            // textBox_dir
+            // 
+            this.textBox_dir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_dir.Location = new System.Drawing.Point(112, 20);
+            this.textBox_dir.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox_dir.Name = "textBox_dir";
+            this.textBox_dir.Size = new System.Drawing.Size(696, 42);
+            this.textBox_dir.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(821, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 62);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(924, 7);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 62);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "计算行数";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(27, 218);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(33, 33);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1225, 505);
+            this.webBrowser1.TabIndex = 4;
+            // 
+            // textBox_ignore
+            // 
+            this.textBox_ignore.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_ignore.Location = new System.Drawing.Point(112, 77);
+            this.textBox_ignore.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox_ignore.Name = "textBox_ignore";
+            this.textBox_ignore.Size = new System.Drawing.Size(972, 42);
+            this.textBox_ignore.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-2, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "不参与";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-2, 138);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 30);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "仅统计";
+            // 
+            // textBox_only
+            // 
+            this.textBox_only.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_only.Location = new System.Drawing.Point(112, 133);
+            this.textBox_only.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox_only.Name = "textBox_only";
+            this.textBox_only.Size = new System.Drawing.Size(972, 42);
+            this.textBox_only.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(1094, 122);
+            this.button3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 62);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "自产生";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1271, 743);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_only);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_ignore);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_dir);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_dir;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox_ignore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_only;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountCodeLine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            DialogResult ret = dlg.ShowDialog();
+            if (ret == DialogResult.OK)
+            {
+                this.textBox_dir.Text = dlg.SelectedPath;
+            }
+        }
+
+
+
+        public string[] _ignoreList = null;
+
+        public bool CheckIgnore(string name,bool isFile)
+        {
+            if (this._ignoreList == null)
+                return false;
+
+            foreach (string one in this._ignoreList)
+            {
+                if (one.Trim() == "")
+                    continue;
+
+                if (one.StartsWith("*.")  && isFile==true) // 比较扩展名
+                {
+                    string ex = one.Substring(1);
+                    FileInfo fi = new FileInfo(name);
+                    if (fi.Extension == ex)
+                        return true;
+                }
+
+                if (isFile == false)  //目录的情况
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(name);
+                    if (dirInfo.Name==one)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.ClearHtml();
+            this._ignoreList = null ;
+            string ignore = this.textBox_ignore.Text.Trim();
+            if (string.IsNullOrEmpty(ignore) == false)
+            {
+                this._ignoreList = ignore.Split(',');
+            }
+
+            string dir = this.textBox_dir.Text.Trim();
+            if (string.IsNullOrEmpty(dir) == true)
+            {
+                MessageBox.Show(this, "请先选择目录");
+                return;
+            }
+
+            if (Directory.Exists(dir) == false)
+            {
+                MessageBox.Show(this, "目录[" + dir + "]不存在");
+                return;
+            }
+            this.Print("统计开始",true);
+
+            // 清空
+            this._ht.Clear();
+            this._totalCount = 0;
+            string only = this.textBox_only.Text.Trim();
+            if (string.IsNullOrEmpty(only) == true)
+            {
+                int nRet = this.circulationLine(dir, out string error);
+                if (nRet == -1)
+                {
+                    Print("出错：" + error);
+                    MessageBox.Show(this, error);
+                    return;
+                }
+            }
+            else
+            {
+
+                //DirectoryInfo dirInfo = new DirectoryInfo(dir);
+
+                string[] children = only.Split(new char[] {','});
+                foreach (string child in children)
+                {
+                    string one = child.Replace('~','\\');
+                    string theDir = dir + "\\" + one;
+                    int nRet = this.circulationLine(theDir, out string error);
+                    if (nRet == -1)
+                    {
+                        Print("出错：" + error);
+                        MessageBox.Show(this, error);
+                        return;
+                    }
+                }
+
+            }
+
+
+            this.Print("统计完成，共计" + this._totalCount.ToString(), true);
+        }
+
+        Hashtable _ht = new Hashtable();
+        long _totalCount = 0;
+
+        public int circulationLine(string dir, out string error)
+        {
+            int nRet = 0;
+            error = "";
+
+            DirectoryInfo dirInfo = new DirectoryInfo(dir);
+
+            // 列出所有文件
+            FileInfo[] files = dirInfo.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                long lineCount = 0;
+                string filePath = file.FullName;
+
+                // 检查是否为过滤的文件
+                if (this.CheckIgnore(filePath,true)==true) 
+                {
+                    this.Print("忽略" + "\t" + filePath);
+                    continue;
+                }
+
+                StreamReader streamReader = new StreamReader(file.FullName);
+                try
+                {
+                    //判断文件中是否有字符
+                    while (streamReader.Peek() != -1)
+                    {
+                        //读取文件中的一行字符
+                        streamReader.ReadLine();
+                        lineCount++;
+                    }
+                }
+                finally
+                {
+                    streamReader.Close();
+                }
+
+                // 记录下来文件与对应的行数
+                _ht[filePath] = lineCount;
+
+                // 此时就应显示在界面上
+                this.Print(lineCount+"\t"+filePath);
+                this._totalCount += lineCount;
+                Application.DoEvents();
+            }
+
+            // 列出所有目录，递归
+            DirectoryInfo[] childrenDir = dirInfo.GetDirectories();
+
+            foreach (DirectoryInfo one in childrenDir)
+            {
+                // 检查是否为过滤的目录
+                if (this.CheckIgnore(one.FullName, false) == true)
+                {
+                    this.Print("忽略" + "\t" + one.FullName);
+                    continue;
+                }
+
+                nRet = this.circulationLine(one.FullName, out error);
+                if (nRet == -1)
+                    return -1;
+            }
+
+            // 空目录
+            if (files.Length == 0 && childrenDir.Length == 0)
+            {
+                this.Print("空目录" + "\t" + dir);
+            }
+
+            return nRet;
+        }
+
+        public void GetInfo()
+        {
+            // 连接参数
+            this.textBox_dir.Text = Properties.Settings.Default.dir;
+            this.textBox_ignore.Text = Properties.Settings.Default.ignore;
+            this.textBox_only.Text = Properties.Settings.Default.only;
+        }
+
+
+        public void SaveInfo()
+        {
+
+            // 连接参数
+            Properties.Settings.Default.dir = this.textBox_dir.Text.Trim();
+            Properties.Settings.Default.ignore = this.textBox_ignore.Text.Trim();
+            Properties.Settings.Default.only = this.textBox_only.Text.Trim();
+
+            // 保存参数
+            Properties.Settings.Default.Save();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.GetInfo();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.SaveInfo();
+        }
+
+        #region 输出信息浏览器通用函数
+
+        private void Print(string strHtml)
+        {
+            this.Print(strHtml, false);
+        }
+
+        private void Print(string strHtml, bool containerTime)
+        {
+            if (containerTime)
+                strHtml = String.Format("{0}  {1}<br />", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), strHtml);
+            else
+                strHtml = strHtml + "<br />";
+
+            WriteHtml(this.webBrowser1,
+                strHtml);
+        }
+
+        // 不支持异步调用
+        public void WriteHtml(WebBrowser webBrowser,
+            string strHtml)
+        {
+            this.Invoke(new Action(() =>
+            {
+                HtmlDocument doc = webBrowser.Document;
+
+                if (doc == null)
+                {
+                    webBrowser.Navigate("about:blank");
+                    doc = webBrowser.Document;
+                    doc.Write("<pre>");
+                }
+
+                doc.Write(strHtml);
+
+                // 保持末行可见
+                ScrollToEnd(webBrowser);
+            }));
+        }
+
+        public void ClearHtml()
+        {
+            HtmlDocument doc = webBrowser1.Document;
+
+            if (doc == null)
+            {
+                webBrowser1.Navigate("about:blank");
+                doc = webBrowser1.Document;
+            }
+            doc = doc.OpenNew(true);
+            doc.Write("<pre>");
+        }
+
+
+        public static void ScrollToEnd(WebBrowser webBrowser1)
+        {
+            if (webBrowser1.Document != null
+                && webBrowser1.Document.Window != null
+                && webBrowser1.Document.Body != null)
+                webBrowser1.Document.Window.ScrollTo(
+                    0,
+                    webBrowser1.Document.Body.ScrollRectangle.Height);
+        }
+
+        #endregion
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.ClearHtml();
+            this._ignoreList = null;
+            string ignore = this.textBox_ignore.Text.Trim();
+            if (string.IsNullOrEmpty(ignore) == false)
+            {
+                this._ignoreList = ignore.Split(',');
+            }
+
+            string dir = this.textBox_dir.Text.Trim();
+            if (string.IsNullOrEmpty(dir) == true)
+            {
+                MessageBox.Show(this, "请先选择目录");
+                return;
+            }
+
+            if (Directory.Exists(dir) == false)
+            {
+                MessageBox.Show(this, "目录[" + dir + "]不存在");
+                return;
+            }
+            this.textBox_only.Text = "";
+
+            string temp = "";
+
+            DirectoryInfo dirInfo = new DirectoryInfo(dir);
+            // 列出所有文件
+            FileInfo[] files = dirInfo.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string name = file.Name;
+                if (name.LastIndexOf(".") != -1)
+                    name = name.Substring(0,name.LastIndexOf("."));
+
+                if (temp != "")
+                    temp += ",";
+
+                temp += name;
+                
+
+            }
+
+            // 放在仅允许的输入框
+            this.textBox_only.Text = temp;
+        }
+    }
+}
